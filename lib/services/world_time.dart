f899ba74ba4dbb007f2cd96e2ecb8a7cb13716f9
Pramuditha Karunarathna <shemil076@gmail.com>
2022-02-07 12:24:16 +0530

    handle the error that occur if the url is invalid

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -10,21 +10,32 @@ class WorldTime {
   WorldTime({required this.location, required this.flag, required this.url});
 
   Future<void> getTime() async {
-    // make the request
-    Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/$url'));
-    Map data = jsonDecode(response.body);
-    //print(data);
 
-    // get properties from json
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1,3);
-    // print(datetime);
-    // print(offset);
+    try{
+      // make the request
+      Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/$url'));
+      Map data = jsonDecode(response.body);
+      //print(data);
 
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
+      // get properties from json
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'].substring(1,3);
+      // print(datetime);
+      // print(offset);
 
-    // set the time property
-    time = now.toString();
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
+
+      // set the time property
+      time = now.toString();
+    }
+    catch (e) {
+      print('caught exception: $e');
+      time = 'something happens, and could not get time data';
+    }
+
+
+
+
   }
 }
